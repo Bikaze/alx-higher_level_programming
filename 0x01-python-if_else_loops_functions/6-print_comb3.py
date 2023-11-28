cf2369373a34807866c2e0ea7bdeccce248b3d1a
bikaze <clmntmugisha@gmail.com>
2023-11-28 19:04:14 +0200

    change

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(0, 10):
         if i == j:
             pass
         elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
+            print("{0}{1}".format(i, j))
         else:
-            print("{}{}".format(i, j), end=", ")
+            print("{0}{1}".format(i, j), end=", ")
