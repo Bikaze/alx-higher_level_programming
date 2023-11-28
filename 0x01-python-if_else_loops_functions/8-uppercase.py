0edfe39e5c56fe254d844b663cd7320d0da72f67
bikaze <clmntmugisha@gmail.com>
2023-11-28 19:30:16 +0200

    change

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(word):
+    new = ""
+    for c in word:
+        if ord(c) >= 97 and ord(c) <= 123:
+            new += chr(ord(c) - 32)
+        else:
+            new += c
+    return new
+
+
+print(uppercase("Best School 98 Battery street"))
