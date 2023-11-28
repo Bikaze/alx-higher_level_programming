074f93e2244499260c518305ffc45c34277d7d37
bikaze <clmntmugisha@gmail.com>
2023-11-28 13:56:22 +0200

    change

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/*
+ *This code implements the Floyd's Tortoise and Hare algorithm
+ *used to detect cycles in a linked list
+ */
+
 /**
   *check_cycle - checks whether the linked list has a circle
   *@list: first node of the list
@@ -11,11 +16,11 @@ int check_cycle(listint_t *list)
 	listint_t *hare = list;
 	listint_t *tortoise = list;
 
-	while(hare != NULL && hare->next != NULL)
+	while (hare != NULL && hare->next != NULL)
 	{
 		tortoise = tortoise->next;
 		hare = hare->next->next;
-		if(hare == tortoise)
+		if (hare == tortoise)
 			return (1);
 	}
 	return (0);
