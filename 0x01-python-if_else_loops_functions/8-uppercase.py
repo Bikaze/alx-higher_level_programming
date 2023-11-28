6afb9929a3fc5aa4e121e5313de8e327a45c1752
bikaze <clmntmugisha@gmail.com>
2023-11-28 20:01:15 +0200

    change

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(word):
     for c in word:
         if ord(c) >= 97 and ord(c) <= 123:
-           new = ord(c) - 32
+            new = ord(c) - 32
         else:
             new = ord(c)
         print("{:c}".format(new), end="")
