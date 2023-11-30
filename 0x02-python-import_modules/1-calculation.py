af64976dd286e9ff525309abfb4ac3cbb4e622e0
bikaze <clmntmugisha@gmail.com>
2023-11-30 13:15:02 +0200

    change

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     a = 10
     b = 5
-    add = calc.add
-    div = calc.div
-    sub = calc.sub
-    mul = calc.mul
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} + {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, mul(a, b)))
