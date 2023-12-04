2226aa96a7598afee20dcc30bc2b4726caff1d64
bikaze <clmntmugisha@gmail.com>
2023-12-04 18:51:03 +0200

    change

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    box = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        for i in range(len(my_list)):
+            if i == idx:
+                pass
+            else:
+                box.append(my_list[i])
+        my_list[:] = box
+        return box
