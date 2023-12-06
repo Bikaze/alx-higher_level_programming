1617435ea6390767c464f7a9acb57608d6c7001e
bikaze <clmntmugisha@gmail.com>
2023-12-06 14:54:48 +0200

    change

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new = dict(a_dictionary)
-    for k, v in new.items():
-        v *= 2
+    for k in list(new):
+        new[k] *= 2
     return new
