145f662aa1e185367a731f0968cb73300c79a012
bikaze <clmntmugisha@gmail.com>
2023-12-19 23:15:50 +0200

    change

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A simple class
+"""
+
+
+class Square:
+    """Class containing the size attribute"""
+    def __init__(self, size):
+        """Instatiation of Square class"""
+        self.__size = size
