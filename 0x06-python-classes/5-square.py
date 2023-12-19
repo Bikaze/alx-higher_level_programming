2dea1eab3b3f79e757906004f03fc033ab9c9634
bikaze <clmntmugisha@gmail.com>
2023-12-20 00:33:40 +0200

    change

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A simple class
+"""
+
+
+class Square:
+    """Class containing the size attribute"""
+    def __init__(self, size=0):
+        """initiator function"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """setter of Square class"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """compute the area of the square"""
+        return (self.__size)**2
+
+    def my_print(self):
+        """print a square using the '#' character"""
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
