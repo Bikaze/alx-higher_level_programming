f20cacae8a7327c5ddf0fc5db62a26a2d6760c1f
bikaze <clmntmugisha@gmail.com>
2024-01-09 16:24:34 +0200

    change

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This file contains the 'Student' class definition
+"""
+
+
+class Student:
+    """
+    A class to represent a student.
+
+    Attributes
+    ----------
+    first_name : str
+        The first name of the student
+    last_name: str
+        The last name of the student
+    age : int
+        The age of the student.
+
+    Methods
+    -------
+    __init__(name, age)
+        Constructor method to initialize the dog with a name and age.
+    to_json()
+        retrieves a dictionary representation of a Student instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return vars(self)
