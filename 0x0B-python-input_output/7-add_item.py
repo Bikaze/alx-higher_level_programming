72fdd16e9315835505921ea3280d821103fb287e
bikaze <clmntmugisha@gmail.com>
2024-01-09 13:34:51 +0200

    change

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
 try:
     existing_matrix = load_from_json_file('add_item.json')
-except:
+except FileNotFoundError:
     save_to_json_file(args, 'add_item.json')
 else:
     save_to_json_file(existing_matrix + args, 'add_item.json')
