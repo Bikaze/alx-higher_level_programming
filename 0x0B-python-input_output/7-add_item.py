de332b4dcf8214d72cdf32c6e18170914c556f1a
bikaze <clmntmugisha@gmail.com>
2024-01-09 13:33:10 +0200

    change

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script adds all arguments to a Python list, and then saves them
+to the file 'add_item.json"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+args = sys.argv[1:]
+try:
+    existing_matrix = load_from_json_file('add_item.json')
+except:
+    save_to_json_file(args, 'add_item.json')
+else:
+    save_to_json_file(existing_matrix + args, 'add_item.json')
