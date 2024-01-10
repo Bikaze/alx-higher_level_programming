53a07753a34ebb554b7f5d2debbb73b0d88fab1b
bikaze <clmntmugisha@gmail.com>
2024-01-10 16:28:16 +0200

    change

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class Square(Rectangle)
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class representing a Square. It inherits from Rectangle Class"""
+
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self._size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self._size}/{self_size}"
