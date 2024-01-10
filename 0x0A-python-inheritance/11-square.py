443446399ddd89a6fe181fd3145c616c2dfd5271
bikaze <clmntmugisha@gmail.com>
2024-01-10 16:29:55 +0200

    change

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,4 +15,4 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def __str__(self):
-        return f"[Square] {self._size}/{self_size}"
+        return f"[Square] {self._size}/{self._size}"
