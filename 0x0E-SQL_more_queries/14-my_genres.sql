ed0e69a6a6860823e0d9b81eee79c7b0050ce370
bikaze <clmntmugisha@gmail.com>
2024-02-15 02:12:20 +0200

    change

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,6 @@
 -- This script lists all genres of the show 'Dexter'
-SELECT name FROM tv_genres
-JOIN tv_show_genres
-ON id = tv_show_genres.genre_id
-WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
-GROUP BY name
-ORDER BY name;
+SELECT tv_genres.name FROM tv_genres, tv_show_genres, tv_shows
+WHERE tv_genres.id = tv_show_genres.genre_id
+AND tv_show_genres.show_id = tv_shows.id
+AND tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
