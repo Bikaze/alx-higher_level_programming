d988a66ef5053c10d2e4dcbe3525b0dfe049be29
bikaze <clmntmugisha@gmail.com>
2024-03-12 20:37:39 +0200

    Create 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
+      }
+    }
+  }
+}
+
+module.exports = Square;
