0c2ad515944def9d627c26f03618fda6a4f5216b
bikaze <clmntmugisha@gmail.com>
2024-03-18 00:39:50 +0200

    Created 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -5,12 +5,13 @@ import sys
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
-# from urllib.parse import quote_plus
-# 
-# pwd = quote_plus("@Mcclent8")
-# url = f"mysql://bikaze:{pwd}@localhost:3306/hbtn_0e_6_usa"
-# engine = create_engine(url)
+from urllib.parse import quote_plus
+
+pwd = quote_plus("@Mcclent8")
+url = f"mysql://bikaze:{pwd}@localhost:3306/hbtn_0e_6_usa"
+engine = create_engine(url)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(url)
+# engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
