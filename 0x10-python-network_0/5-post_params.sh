22fd78b7ea42f27f86923c7bef00adcb9165fd87
bikaze <clmntmugisha@gmail.com>
2024-03-27 16:46:36 +0200

    Updated task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a host, and submits a POST request with parameters
-curl -s -X POST -d \{"email": "test@gmail.com", "subject": "I will always be here for PLD"\} "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
