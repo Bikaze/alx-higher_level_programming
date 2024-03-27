629e19200deb269ca59a6e11cba9140f7e0d3817
bikaze <clmntmugisha@gmail.com>
2024-03-27 16:21:08 +0200

    Updated task 3 again

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes is a URL, and displays all methods accepted by the server
-curl -sX OPTIONS "$1" | grep "Allow" | cut -d " " -f2-
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
