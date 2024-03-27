1585ccf727e41ee8a556552cf13ecabea49ca024
bikaze <clmntmugisha@gmail.com>
2024-03-27 17:52:14 +0200

    Attempted the interview question

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This algorithm finds a peak in a list of unsorted numbers"""
+
+def find_peak(list_int):
+    """This function takes a list of numbers and returns one peak"""
+    if len(set(list_int)) == 1:
+        return list_int[0]
+    for i in range(len(list_int)):
+        if not list_int[i-1] or list_int[i] > list_int[i-1]:
+            if not list_int[i+1] or list_int[i] > list_int[i+1]:
+                return list_int[i]
+                break;
