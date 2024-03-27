53545902e82733b29e94cef9a0a04f35b9dcac8a
bikaze <clmntmugisha@gmail.com>
2024-03-27 19:27:27 +0200

    Update the last task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request and causes the server to respond by "You got me!"
-curl -sw "You got me!" -o /dev/null 0.0.0.0:5000/catch_me
+curl -sLw "You got me!" -o /dev/null 0.0.0.0:5000/catch_me
