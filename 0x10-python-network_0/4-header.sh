2f57189391dc348faa3a2021820986a8a37004fd
bikaze <clmntmugisha@gmail.com>
2024-03-27 16:43:48 +0200

    Created task 5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes is a URL, and displays all methods accepted by the server
+# This script takes in a host, and submits a GET request with a header
 curl -sH "X-school-User-Id: 98" "$1"
