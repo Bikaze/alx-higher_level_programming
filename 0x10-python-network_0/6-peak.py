18ebb3a3b50b6aaa8d2a79446514a190f9e6197b
bikaze <clmntmugisha@gmail.com>
2024-03-27 17:55:22 +0200

    Updated the Interview qnt

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This algorithm finds a peak in a list of unsorted numbers"""
 
+
 def find_peak(list_int):
     """This function takes a list of numbers and returns one peak"""
     if len(set(list_int)) == 1:
@@ -9,4 +10,4 @@ def find_peak(list_int):
         if not list_int[i-1] or list_int[i] > list_int[i-1]:
             if not list_int[i+1] or list_int[i] > list_int[i+1]:
                 return list_int[i]
-                break;
+                break
