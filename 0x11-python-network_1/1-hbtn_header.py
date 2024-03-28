0f913b902842c248e6bbb655e746385850ee279f
bikaze <clmntmugisha@gmail.com>
2024-03-28 22:56:31 +0200

    Updated for the last time task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # This python script takes in a URL, sends a request to the URL
-from urllib.request import Request, urlopen
+import urllib.request
 from sys import argv
 
 if __name__ == "__main__":
-    req = Request(argv[1])
-
-    with urlopen(req) as response:
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
         print(dict(response.headers).get("X-Request-Id"))
