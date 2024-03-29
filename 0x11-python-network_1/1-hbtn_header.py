e3d03d2ae0accd131c3b593c8802b144e4c730e6
bikaze <clmntmugisha@gmail.com>
2024-03-29 08:41:49 +0200

    resolving some errors

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This python script takes in a URL, sends a request to the URL and displays 
+"""This python script takes in a URL, sends a request to the URL and displays
  the value of the X-Request-Id variable found in the header of the response
 """
 if __name__ == "__main__":
@@ -8,4 +8,4 @@ if __name__ == "__main__":
 
     req = urllib.request.Request(argv[1])
     with urllib.request.urlopen(req) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(response.getheader("X-Request-Id"))
