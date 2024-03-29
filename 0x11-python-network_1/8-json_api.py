f0b9387d415611d12f3c1d8be05c6152f09f85a3
bikaze <clmntmugisha@gmail.com>
2024-03-29 12:58:14 +0200

    Updated 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script takes in a letter and sends a POST request to 
+"""This script takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
 if __name__ == "__main__":
