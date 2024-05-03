cceff82093b67617830f4852c95f54a1590e0736
Bikaze <clmntmugisha@gmail.com>
2024-05-03 18:27:14 +0200

    Created 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,7 +1,12 @@
-//This code fetches from an API using user input
+//This script fetches from and API using data from user input
 
 $('document').ready(function () {
 	const url = "https://hellosalut.stefanbohacek.dev/?";
+	$('input#language_code').on('keydown', function(e) {
+		if (e.which === 13 && $(this).is(':focus')) {
+			$('input#btn_translate').click();
+		}
+	});
   	$('input#btn_translate').click(function () {
 		const usr_input = $("input#language_code").val();
 		$.get(url+"lang="+usr_input, function (data) {
